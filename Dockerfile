ab346374c28b64ee01394d7445ad7fe12019a5d8
GeronimoCR <geronimocorters@gmail.com>
2025-11-04 22:33:56 -0600

    por favor

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,6 @@
 # Etapa 1: construir el entorno con dependencias
 FROM python:3.12.6 AS builder
-
-ENV PYTHONUNBUFFERED=1 \
-    PYTHONDONTWRITEBYTECODE=1
-
+ENV PYTHONUNBUFFERED=1 PYTHONDONTWRITEBYTECODE=1
 WORKDIR /app
 
 RUN python -m venv /opt/venv
@@ -12,19 +9,14 @@ ENV PATH="/opt/venv/bin:$PATH"
 COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Etapa 2: imagen final más ligera
+# Etapa 2: imagen final
 FROM python:3.12.6-slim
-
 WORKDIR /app
 COPY --from=builder /opt/venv /opt/venv
 ENV PATH="/opt/venv/bin:$PATH"
 
 COPY . .
-
-# Fly espera que la app escuche en el puerto 8080
 EXPOSE 8080
 
-# Usa waitress para servir la app en el puerto 8080
 CMD ["waitress-serve", "--listen=0.0.0.0:8080", "app:app"]
-
 
--- app.py
+++ app.py
@@ -5,13 +5,11 @@ from Scorecard.Scorecard import scorecard_bp
 from Analisis_Energetico.Analisis_Energetico import analisisE_bp
 from Predictores.Predictores import predictores_bp
 from Chatbot.chatbot import chatbot_bp
-from waitress import serve
-
 
 app = Flask(__name__)
-
 app.secret_key = 'd390e3824fc22f6f5acb98fd0910c9da9acd80ec46b2b7e0'
-#BLUEPRINTS (Diversos modulos para las diversas funciones)
+
+# BLUEPRINTS
 app.register_blueprint(monitoreo_bp, url_prefix='/Monitoreo_Energetico')
 app.register_blueprint(PDFprev_bp, url_prefix='/PDFprev')
 app.register_blueprint(scorecard_bp, url_prefix='/Scorecard')
@@ -19,19 +17,10 @@ app.register_blueprint(predictores_bp, url_prefix='/Predictores')
 app.register_blueprint(analisisE_bp, url_prefix='/AnalisisE')
 app.register_blueprint(chatbot_bp, url_prefix='/chatbot')
 
-
-#RUTA DE INDICE PRINCIPAL
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
-if __name__ == "__main__":
-    import os
-    from waitress import serve
-
-    port = int(os.environ.get("PORT", 8080))
-    serve(app, host="0.0.0.0", port=port, threads=6)
 
     
     
